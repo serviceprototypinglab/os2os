eac398476a9d3f351d61daa2d281ef9dc3fd94c3
ramz <ramz@zhaw.ch>
2017-10-25 17:57:28 +0200

    Starting with upData

--- cmd/upData.go
+++ cmd/upData.go
@@ -15,7 +15,7 @@ package cmd
 
 import (
 	"fmt"
-
+	"os/exec"
 	"github.com/spf13/cobra"
 )
 
@@ -31,6 +31,7 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("upData called")
+		upData(cmd, args)
 	},
 }
 
@@ -46,4 +47,33 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// upDataCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
+func upData(cmd *cobra.Command, args []string) {
+	loginCluster(ClusterTo, UsernameFrom, PasswordFrom)
+
+	//Get pods from deployment
+	deploymentName := "A"
+	//TODO GET ALL THE VOLUME TO MIGRATE
+	podName := getPodName(deploymentName)
+	//Copy the data there
+	upDataToVolume(podName, podName, podName)
+}
+
+func getPodName(deploymentName string) string {
+	return deploymentName
+}
+
+func upDataToVolume(podName, path, mountPath string) {
+	a := "oc rsync " +  path + "/data"  +  " " + podName + ":" + mountPath
+	fmt.Println(a)
+	cmdUpData := exec.Command("oc", "rsync", path + "/data", podName + ":" + mountPath)
+	cmdUpOut, err := cmdUpData.Output()
+	if err != nil {
+		fmt.Println("Error migrating " + a)
+		fmt.Println(err)
+	} else {
+		fmt.Println(string(cmdUpOut))
+	}
 }
