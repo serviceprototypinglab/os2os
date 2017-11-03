a2c41e7643bdb2f156f0186032bfd07e555faa9d
ramz <ramz@zhaw.ch>
2017-11-03 09:40:38 +0100

    migrate and migrate templates

--- cmd/migrateTemplate.go
+++ cmd/migrateTemplate.go
@@ -1,5 +1,4 @@
-// Prototype (☺) 2017 Manuel Ramirez Lopez <ramz@zhaw.ch>
-// Copyright (©) 2017 Zürcher Hochschule für Angewandte Wissenschaften
+// Copyright © 2017 Manuel Ramirez Lopez <ramz@zhaw.ch>
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -20,37 +19,39 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// migrateCmd represents the migrate command
-var migrateCmd = &cobra.Command{
-	Use:   "migrate",
-	Short: "Full migration",
-	Long: `All the steps in a full migration`,
+// migrateTemplateCmd represents the migrateTemplate command
+var migrateTemplateCmd = &cobra.Comm{
+	Use:   "migrateTemplate",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("migrate called")
-		migrate(cmd, args)
+		fmt.Println("migrateTemplate called")
+		migrateTemplate(cmd, args)
 	},
 }
 
 func init() {
-	RootCmd.AddCommand(migrateCmd)
+	RootCmd.AddCommand(migrateTemplateCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// migrateCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// migrateTemplateCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// migrateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// migrateTemplateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 
-func migrate(cmd *cobra.Command, args []string){
+func migrateTemplate(cmd *cobra.Command, args []string){
 	export(cmd, args)
-	exportData(cmd, args)
 	up(cmd, args)
-	upData(cmd, args)
-	downData(cmd, args)
 	down(cmd, args)
 }
